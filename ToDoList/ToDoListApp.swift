50c5613adb9d380f6275f08a8a887cd3de31ceba
Eunah <eunahmaumbe@gmail.com>
2025-07-11 12:53:32 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Scholar on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
